8adc6f12d501dd9b1ed4426c3d0ad22ffdd686dd
vitaliidox <72409488+vitaliidox@users.noreply.github.com>
2021-05-14 13:23:30 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+let films = document.querySelector("#films")
+let val = document.querySelector("#rating")
+let numRating = document.querySelector("#numRating")
+let clickDown = false
+
+fetch(`http://api.themoviedb.org/3/movie/popular?api_key=2e901364c3d103dcb00ced520e9bca3c`)
+.then(response => response.json())
+.then(data => {
+    val.addEventListener('mousedown', function(){
+        clickDown = true
+    })
+    val.addEventListener('mousemove', function(){
+        if(clickDown == true){
+        val = document.querySelector("#rating").value
+        numRating.innerHTML=`${val}`     
+    }}
+    )
+    val.addEventListener('mouseup', function(){
+        clickDown = false
+        films.innerHTML=""
+        new Rage(data).sortRange()
+    })
+})
+
+class Rage{
+    constructor(data){
+        this.data = data
+    }
+    sortRange(){
+        for(let i=0; i<this.data.results.length;i++){
+            if(Math.floor(this.data.results[i].vote_average) == Math.floor(val)){
+                films.innerHTML+=`
+                <div>
+                    <h2>${this.data.results[i].title}</h2>
+                    <h4>Rating:${this.data.results[i].vote_average}</h4>
+                    <p>${this.data.results[i].overview}</p>
+                </div>
+                `
+            }
+        }        
+    }
+}
